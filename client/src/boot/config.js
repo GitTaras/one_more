cc77b48d00b93a3be187717f6e367524b8374956
Taras <taras.sholtys@gmail.com>
2020-02-25 15:40:14 +0200

    add redux-saga and other...

--- client/src/boot/config.js
+++ client/src/boot/config.js
@@ -1,14 +1,20 @@
 import { createStore, applyMiddleware /*compose*/ } from 'redux';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from '../sagas/rootSaga';
+import { requestsPromiseMiddleware } from 'redux-saga-requests';
 import combinedReducers from '../reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 function configureStore() {
-  const middleware = [thunk];
-  return createStore(
+  const sagaMiddleware = createSagaMiddleware();
+  const middleware = [thunk, sagaMiddleware];
+  const store = createStore(
     combinedReducers,
-    composeWithDevTools(applyMiddleware(...middleware))
+    composeWithDevTools(applyMiddleware(requestsPromiseMiddleware(), ...middleware)),
   );
+  sagaMiddleware.run(rootSaga, store.dispatch);
+  return store;
 }
 
 export default configureStore();
