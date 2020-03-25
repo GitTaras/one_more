6e12481c9bd373d363d96e9050958e9051278093
taras <taras.sholtys@gmail.com>
2020-03-25 18:29:55 +0200

    fixed autocomplete text area

--- src/components/PostForm/PostForm.js
+++ src/components/PostForm/PostForm.js
@@ -5,52 +5,50 @@ import { postSchema } from '../../utils/validators';
 import { Button } from 'antd';
 import { postChatMessage } from '../../store/messages/messagesActions';
 import styles from './PostForm.module.css';
-import Autocomplete from 'react-autocomplete';
 import { getAutocompleteUsers } from '../../store/autocomplete/autocompleteActions';
-import inputStyle from '../UI/Input/Input.module.css';
+import ReactTextareaAutocomplete from '@webscopeio/react-textarea-autocomplete';
+import '@webscopeio/react-textarea-autocomplete/style.css';
+
+const AutocompleteItem = ({ entity: { username } }) => <div>{username}</div>;
+const AutocompleteLoading = () => <div>Loading ...</div>;
+
+const AutocompleteTextArea = props => {
+  const autocompleteUsernameDataProvider = async searchStr => {
+    return await getAutocompleteUsers(searchStr);
+  };
+
+  const textAreaChangeHandler = (e) => {
+    props.onChange(e);
+  }
+
+  const autocompleteTriggers = {
+    '@': {
+      component: AutocompleteItem,
+      dataProvider: autocompleteUsernameDataProvider,
+      output: ({ username }) => `@${username}`,
+    },
+  };
+
+  const { value, name } = props;
+
+  return (
+    <ReactTextareaAutocomplete
+      loadingComponent={AutocompleteLoading}
+      onChange={textAreaChangeHandler}
+      rows="5"
+      value={value}
+      trigger={autocompleteTriggers}
+      minChar={2}
+      name={name}
+    />
+  );
+};
 
 const PostForm = ({ isLoading, post }) => {
-  const [mentions, setMentions] = useState([]);
-  const [isOpenAutocomplete, setOpenAutocomplete] = useState(false);
-
   const handleSubmit = values => {
     if (!isLoading) {
       post({ message: values.message });
     }
-  };
-
-  const getMentions = async name => {
-    const usernames = await getAutocompleteUsers(name);
-    setMentions(usernames);
-  };
-
-  const checkForMentions = value => {
-    //todo write correct reg exp for @
-    const names = value.match(/(^@.\w+)/);
-    if (names) {
-      setOpenAutocomplete(true);
-      getMentions(names[0].substring(1));
-      return;
-    }
-    setOpenAutocomplete(false);
-  };
-
-  const renderMyInput = props => {
-    const { error } = props;
-    return (
-      <>
-        <input
-          className={error ? inputStyle.inputDanger : ''}
-          name="message"
-          type="text"
-          id="message"
-          placeholder="type your message"
-          maxLength={250}
-          {...props}
-        />
-        {error && <span className={inputStyle.error}>{error}</span>}
-      </>
-    );
   };
 
   return (
@@ -65,30 +63,11 @@ const PostForm = ({ isLoading, post }) => {
       >
         {({ handleSubmit, handleChange, values, errors, setFieldValue }) => (
           <form onSubmit={handleSubmit} className={styles.chatForm}>
-            <Autocomplete
-              wrapperProps={{ className: styles.inputContainer }}
-              wrapperStyle={{ display: 'flex', width: '80%' }}
-              isOpen={false}
-              renderInput={renderMyInput}
-              autoFocus={true}
-              getItemValue={item => item.username}
-              inputProps={{
-                error: errors.message,
-              }}
-              items={mentions}
-              renderItem={(item, isHighlighted) => (
-                <div key={item.id} style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
-                  {item.username}
-                </div>
-              )}
+            <AutocompleteTextArea
               value={values.message}
-              onChange={e => {
-                handleChange(e);
-                checkForMentions(e.target.value);
-              }}
-              onSelect={mantion => setFieldValue('message', values.message.concat(mantion), false)}
-              open={isOpenAutocomplete}
+              onChange={handleChange}
               error={errors.message}
+              name="message"
             />
             <div>
               <Button htmlType="submit" type="primary" size="large" loading={isLoading}>
